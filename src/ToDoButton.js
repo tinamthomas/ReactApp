64bbf3174a5fbcdf1f9ed33fd16ca67930801cc2
tiffany <tiffanymarythomas1@gmail.com>
2021-09-11 23:32:41 +0530

    Displayed to Do list

--- src/ToDoButton.js
+++ src/ToDoButton.js
@@ -2,9 +2,9 @@ import { useState } from "react"
 
 const ToDoButton=()=>{
     const[inputText,setInputText]=useState('')
-    const[displayText,setDisplayText]=useState('')
+    const[toDoList,setToDoList]=useState([])
     const onButtonClick=()=>{
-        setDisplayText(inputText)
+        setToDoList([...toDoList,inputText])
         setInputText('')
     }
     const listenToText=(event)=>{
@@ -14,7 +14,9 @@ const ToDoButton=()=>{
         <div>
             <input type="text" onChange={listenToText} value={inputText}></input>
             <button onClick={onButtonClick}>Click</button>
-            {displayText.length!==0&&<div>{displayText}</div>}
+            <div><ul>{toDoList.map(todo=>{
+                    return <li>{todo}</li>
+            })}</ul></div>
         </div>
     )
 }
