a4e570e3fb080d047d1acf23b6c7e7637bc2b679
tiffany <tiffanymarythomas1@gmail.com>
2021-10-02 23:24:49 +0530

    Added check behaviour to todoitem

--- src/todoItem.js
+++ src/todoItem.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 const ToDoItem=(props)=>{
+    const [checked,setChecked]=useState(false);
+    function updateCheck(){
+        setChecked(!checked);
+    }
     return(
-       <li>{props.todo}</li>
+        <div>
+       <input type="checkbox" onChange={updateCheck}></input>
+        {checked&&<del><label>{props.todo}</label></del>}
+        {!checked&&<label>{props.todo}</label>}
+        </div>
     )
 }
 export default ToDoItem;
