eb87b66e0cac0d667df82e648be7345a4dc33c70
tiffany <tiffanymarythomas1@gmail.com>
2021-10-03 15:19:18 +0530

    Made lists editable

--- src/todoItem.js
+++ src/todoItem.js
@@ -2,14 +2,28 @@ import { useState } from "react";
 
 const ToDoItem=(props)=>{
     const [checked,setChecked]=useState(false);
+    const[isEditing,setIsEditing]=useState(false)
+    const [currentItem,setCurrentItem]=useState(props.todo)
     function updateCheck(){
         setChecked(!checked);
+    }
+    function editList(){
+        if(!checked)
+        setIsEditing(!isEditing);
+    }
+    function changeItem(event){
+        setCurrentItem(event.target.value);
     }
     return(
         <div>
        <input type="checkbox" onChange={updateCheck}></input>
-        {checked&&<del><label>{props.todo}</label></del>}
-        {!checked&&<label>{props.todo}</label>}
+       {checked?
+       <del><label>{props.todo}</label></del>:
+       !isEditing?
+                <label>{currentItem}</label>:
+                <input type="text" value={currentItem} onChange={changeItem}></input>}
+       
+        <input type="button" value={isEditing?"Save":"Edit"} onClick={editList}></input>
         </div>
     )
 }
