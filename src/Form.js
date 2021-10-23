ab29b245f6fe1f6e3c8a14653f45d016aea67268
tiffany <tiffanymarythomas1@gmail.com>
2021-10-23 21:35:03 +0530

    unit tested form with two inputs

--- src/Form.js
+++ src/Form.js
@@ -2,15 +2,20 @@ import { useState } from "react/cjs/react.development"
 
 const Form=(props)=>{
     const [currentName,setCurrentName]=useState(null)
+    const [currentAge,setCurrentAge]=useState(null)
     const changedName=(event)=>{
         setCurrentName(event.target.value)
     }
+    const changedAge=(event)=>{
+        setCurrentAge(event.target.value)
+    }
     const submitName=()=>{
-        props.parentCallback(currentName)
+        props.parentCallback({name:currentName,age:currentAge})
     }
     return(
         <div>
             <input type="text" onChange={changedName} data-testid="textInput"></input>
+            <input type="text" onChange={changedAge} data-testid="ageInput"></input>
             <button type="submit" onClick={submitName} data-testid="saveButton">Save</button>
         </div>
     )
--- src/tests/form.test.js
+++ src/tests/form.test.js
@@ -2,13 +2,14 @@ import {render, fireEvent, screen, waitFor} from '@testing-library/react'
 import RenderForm from '../RenderForm'
 import Form from "./../Form"
 test.only('input validation',async()=>{
+    const name="A"
+    const age="1"
     const parentCallback=jest.fn()
     render(<Form parentCallback={parentCallback}></Form>)
 
-    const input = screen.getByTestId("textInput")
-    fireEvent.change(input, { target: { value: '123' } })
-    const save=screen.getByTestId("saveButton");
-    fireEvent.click(save)
+    fireEvent.change(screen.getByTestId("textInput"), { target: { value: name} })
+    fireEvent.change(screen.getByTestId("ageInput"), { target: { value: age } })
+    fireEvent.click(screen.getByTestId("saveButton"))
 
-    expect(parentCallback).toHaveBeenLastCalledWith('123')
+    expect(parentCallback).toHaveBeenLastCalledWith({name,age})
 })
